26bbdc54b3fc88334793040c6a6fefa980dc1c11
Baekhyeon <baeks215@gmail.com>
2026-06-17 01:58:29 +0100

    feat: focus for fullscreen

--- crates/vexor-gui/src/lib.rs
+++ crates/vexor-gui/src/lib.rs
@@ -31,6 +31,7 @@ pub fn run(
                 rx,
                 exports: Vec::new(),
                 version: 0,
+                focus: None,
             }))
         }),
     )
@@ -40,6 +41,7 @@ struct GuiApp {
     rx: mpsc::Receiver<Vec<NamedSvg>>,
     exports: Vec<NamedSvg>,
     version: u64,
+    focus: Option<usize>,
 }
 
 impl eframe::App for GuiApp {
@@ -52,24 +54,75 @@ impl eframe::App for GuiApp {
             self.exports = exports;
             self.version = self.version.wrapping_add(1);
         }
+        if let Some(idx) = self.focus {
+            if idx < self.exports.len() {
+                self.focus_view(ui, idx);
+                return;
+            }
+            self.set_focus(ui, None);
+        }
         let image_width = ui.available_width() * IMAGE_WIDTH_FRACTION;
         egui::ScrollArea::vertical().show(ui, |ui| {
             ui.vertical_centered(|ui| {
-                for ex in &self.exports {
+                for idx in 0..self.exports.len() {
+                    let (name, svg_bytes) = {
+                        let ex = &self.exports[idx];
+                        (ex.name.clone(), ex.svg.as_bytes().to_vec())
+                    };
                     ui.group(|ui| {
                         ui.set_width(image_width);
                         ui.vertical_centered(|ui| {
-                            ui.heading(&ex.name);
-                            let uri = format!("bytes://v{}_{}.svg", self.version, ex.name);
+                            let mut focus = false;
+                            ui.horizontal(|ui| {
+                                ui.heading(&name);
+                                focus = ui.button("Focus").clicked();
+                            });
+                            let uri = format!("bytes://v{}_{}.svg", self.version, name);
                             ui.add(
-                                egui::Image::from_bytes(uri, ex.svg.as_bytes().to_vec())
+                                egui::Image::from_bytes(uri, svg_bytes)
                                     .fit_to_fraction(egui::Vec2::new(1.0, f32::INFINITY)),
                             );
+                            if focus {
+                                self.set_focus(ui, Some(idx));
+                            }
                         });
                     });
                     ui.add_space(8.0);
                 }
             });
+        });
+    }
+}
+
+impl GuiApp {
+    /// Enter/exit focus mode, taking the window in/out of real monitor fullscreen.
+    fn set_focus(&mut self, ui: &egui::Ui, target: Option<usize>) {
+        self.focus = target;
+        ui.ctx()
+            .send_viewport_cmd(egui::ViewportCommand::Fullscreen(target.is_some()));
+    }
+
+    fn focus_view(&mut self, ui: &mut egui::Ui, idx: usize) {
+        let escape = ui.input(|i| i.key_pressed(egui::Key::Escape));
+        let mut exit = escape;
+        ui.horizontal(|ui| {
+            exit |= ui.button("Exit focus").clicked();
+        });
+        if exit {
+            self.set_focus(ui, None);
+            return;
+        }
+
+        let (name, svg_bytes) = {
+            let ex = &self.exports[idx];
+            (ex.name.clone(), ex.svg.as_bytes().to_vec())
+        };
+        let uri = format!("bytes://focus_v{}_{}.svg", self.version, name);
+        ui.centered_and_justified(|ui| {
+            ui.add(
+                egui::Image::from_bytes(uri, svg_bytes)
+                    .fit_to_fraction(egui::Vec2::new(1.0, 1.0)),
+            );
         });
     }
 }
